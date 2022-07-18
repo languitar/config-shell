6c2b83b3fe9d70a525779fdd7980cee77eccfa1b
Johannes Wienke <johannes.wienke@plan.one>
2022-07-18 15:37:49 +0200

    Add wayland support to copy and paste functions

--- home/.config/fish/functions/c.fish
+++ home/.config/fish/functions/c.fish
@@ -1,5 +1,7 @@
 function c
-    if which xclip 2> /dev/null > /dev/null
+    if which wl-copy 2> /dev/null > /dev/null
+        wl-copy
+    else if which xclip 2> /dev/null > /dev/null
         xclip -selection clipboard
     else if which pbcopy 2> /dev/null > /dev/null
         pbcopy
--- home/.config/fish/functions/p.fish
+++ home/.config/fish/functions/p.fish
@@ -1,5 +1,7 @@
 function p
-    if which xclip 2> /dev/null > /dev/null
+    if which wl-paste 2> /dev/null > /dev/null
+        wl-paste
+    else if which xclip 2> /dev/null > /dev/null
         xclip -selection clipboard -o
     else if which pbcopy 2> /dev/null > /dev/null
         pbpaste
